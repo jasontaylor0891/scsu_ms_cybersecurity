a8c97f3d3aa3d27982f9c73a6a5ccde94aeb88bd
Jason Taylor <funkmusha@hotmail.com>
2021-09-18 13:39:10 -0400

    Added code to calculate FLOPS and IOPS

--- CSC563/Project1/cpu_benchmark.py
+++ CSC563/Project1/cpu_benchmark.py
@@ -26,38 +26,80 @@ class cpu_benchmark(threading.Thread):
             self.number_of_operations = 0
             while self.terminate:
                 self.number_of_operations +=1
-        #print(f"{self.number_of_operations}")
-        
 
+#Display usage for this script
 def display_help():
     print("\nusage: cpu_benchmark.py sample_time number_of_threads\n")
 
+#Function used to mesure wait time.
 def time_thread(sleep_time):
     time.sleep(sleep_time)
 
 def main(test_time, num_threads):
 
-    t1 = cpu_benchmark(False)
-    t2 = cpu_benchmark(False)
-
-    t1.start()
-    t2.start()
-
-    flops_thread = threading.Thread(target=time_thread, args=(int(test_time),))
-    flops_thread.start()
-    flops_thread.join()
-
-    #cpu_benchmark.set_terminate_flag(False)
-    t1.set_terminate_flag(False)
-    t2.set_terminate_flag(False)
-
-    print(t1.get_number_operations())
-    print(t2.get_number_operations())
-    print("done")
-    #t2.start()
-
+    flops = []
+    iops = []
+    f_ops = []
+    i_ops = []
     
     
+    for _ in range(3):
+        
+        #Calculating FLOPS:
+        thread_list = []
+
+        #Create number of threads defined in num_threads from user input.
+        for _ in range(int(num_threads)):
+            threads = cpu_benchmark(True)
+            threads.start()
+            thread_list.append(threads)
+        
+        #Create the timer and wait.  Test time is defined from user input
+        timer_thread = threading.Thread(target=time_thread, args=(int(test_time),))
+        timer_thread.start()
+        timer_thread.join()
+
+        #After test time is over send update the termination flag to stop the counter.
+        for t in thread_list:
+            t.set_terminate_flag(False)
+
+        #Get the number of operations and calculate FLOPS
+        for t in thread_list:
+            f_ops.append(t.get_number_operations())
+            flops.append(t.get_number_operations() / int(test_time))
+
+        
+        #Calculating IOPS:
+
+        thread_list = []
+
+        #Create number of threads defined in num_threads from user input.
+        for _ in range(int(num_threads)):
+            threads = cpu_benchmark(False)
+            threads.start()
+            thread_list.append(threads)
+        
+        #Create the timer and wait.  Test time is defined from user input
+        timer_thread = threading.Thread(target=time_thread, args=(int(test_time),))
+        timer_thread.start()
+        timer_thread.join()
+
+        #After test time is over send update the termination flag to stop the counter.
+        for t in thread_list:
+            t.set_terminate_flag(False)
+
+        #Get the number of operations and calculate IOPS
+        for t in thread_list:
+            i_ops.append(t.get_number_operations())
+            iops.append(t.get_number_operations() / int(test_time))
+
+    print("FLOPS Output")    
+    print(f"{f_ops}")
+    print(f"{flops}")
+    print("\n\nIOPS Output")
+    print(f"{i_ops}")
+    print(f"{iops}")
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
